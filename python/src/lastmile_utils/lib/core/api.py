9ad9f78514f6d3d8576e1a2b16791647df3e7965
Jonathan Lessinger <jonathan@lastmileai.dev>
2023-11-21 15:22:47 -0500

    Skeleton of utils repo + project + configs

---
+++ python/src/lastmile_utils/lib/core/api.py
@@ -0,0 +1,9 @@
+"""Convenience module for importing from multiple libraries:
+    `import lastmile_utils.lib.core.api as cu`
+
+"""
+
+# pylint: disable=wildcard-import,unused-wildcard-import
+# pyright: reportWildcardImportFromLibrary=false
+from lastmile_utils.lib.core.utils import *
+from lastmile_utils.lib.core.functional import *
