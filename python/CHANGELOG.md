54ae80bfc64c8cd04749ac9400368f35131c6e21
Jonathan Lessinger <jonathan@lastmileai.dev>
2024-01-15 10:55:47 -0500

    [LM-utils][v0.0.21] add + improve decorator utils

--- python/CHANGELOG.md
+++ python/CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.0.21
+
+- add + improve decorator utils
+
 # 0.0.20
 
 - get_logger
--- python/src/lastmile_utils/lib/core/functional.py
+++ python/src/lastmile_utils/lib/core/functional.py
@@ -1,30 +1,84 @@
 import asyncio
-import functools
 import traceback
+from abc import abstractmethod
 from typing import (
     Awaitable,
-    Callable,
-    Concatenate,
-    Generator,
     Iterable,
     Mapping,
     ParamSpec,
+    Protocol,
     Tuple,
     TypeVar,
 )
 
-from result import Err, Ok, Result
+from typing import Callable, Concatenate, ParamSpec, TypeVar
+from result import Result, Ok
 
+
+from result import Err, Ok, Result
 
 # Types
 
-PS = ParamSpec("PS")
 
 TR = TypeVar("TR", covariant=True)
 E = TypeVar("E", covariant=True)
 
 K = TypeVar("K")
 T = TypeVar("T")
+
+PS = ParamSpec("PS")  # Capture the parameters
+PS2 = ParamSpec("PS2")  # Capture the parameters
+PS3 = ParamSpec("PS3")  # Capture the parameters
+T_Return_cov = TypeVar(
+    "T_Return_cov", covariant=True
+)  # constrained on protocol call definition
+T_Return_cov2 = TypeVar("T_Return_cov2", covariant=True)
+T_Return_contra = TypeVar("T_Return_contra", contravariant=True)
+T_Return = TypeVar("T_Return")
+T_Return2 = TypeVar("T_Return2")
+T_Err = TypeVar("T_Err")
+
+
+class T_UnsafeFn(Protocol[PS, T_Return_cov]):
+    """function type returning an arbitrary value"""
+
+    @abstractmethod
+    def __call__(self, *args: PS.args, **kwargs: PS.kwargs) -> T_Return_cov:
+        pass
+
+
+class T_SafeFn(Protocol[PS, T_Return_cov, E]):
+    """function type returning a Result"""
+
+    @abstractmethod
+    def __call__(self, *args: PS.args, **kwargs: PS.kwargs) -> Result[T_Return_cov, E]:
+        pass
+
+
+class T_AsyncUnsafeFn(Protocol[PS, T_Return_cov]):
+    """Async function type returning an arbitrary value"""
+
+    @abstractmethod
+    async def __call__(self, *args: PS.args, **kwargs: PS.kwargs) -> T_Return_cov:
+        pass
+
+
+class T_AsyncSafeFn(Protocol[PS, T_Return_cov, E]):
+    """Async function type returning a Result"""
+
+    @abstractmethod
+    async def __call__(
+        self, *args: PS.args, **kwargs: PS.kwargs
+    ) -> Result[T_Return_cov, E]:
+        pass
+
+
+class T_Decorator(Protocol[PS, T_Return_contra, PS2, T_Return_cov2]):
+    @abstractmethod
+    def __call__(
+        self, func: Callable[PS, T_Return_contra]
+    ) -> Callable[PS2, T_Return_cov2]:
+        pass
 
 
 def ErrWithTraceback(e: Exception, extra_msg: str = "") -> Err[str]:
@@ -84,87 +138,14 @@ def result_reduce_dict_separate(
 async def result_reduce_list_separate_async(
     lst: Iterable[Awaitable[Result[T, str]]]
 ) -> Tuple[list[T], list[str]]:
-    async def _get_result_value(
-        awaitable: Awaitable[Result[T, str]]
-    ) -> Result[T, str]:
+    async def _get_result_value(awaitable: Awaitable[Result[T, str]]) -> Result[T, str]:
         return await awaitable
 
     values = await asyncio.gather(*[_get_result_value(a) for a in lst])
     return result_reduce_list_separate(values)
 
 
-def result_do(
-    func: Callable[
-        Concatenate[PS], Generator[Result[TR, E], TR, Result[TR, E]]
-    ]
-) -> Callable[PS, Result[TR, E]]:
-    """
-    Example:
-
-    import itertools
-    from semantic_retrieval.common.types import DoResult
-
-    def get_result(success: bool, i: int) -> Result[int, str]:
-        if success:
-            out = Ok(42 + i)
-            # print(f"{out=}")
-            return out
-        else:
-            out = Err("magnets" + str(i))
-            return out
-
-
-    def get_my_thing() -> int:
-        return 3
-
-
-    @result_do
-    def result_sum(is1: bool, is2: bool) -> DoResult[int, str]:
-        # Notice that we do no explicit error handling here.
-        r1, r2 = get_result(is1, 1), get_result(is2, 2)
-        x1 = yield r1
-        x2 = yield r2
-
-        some_other_thing = get_my_thing()
-        return Ok(x1 + x2 + some_other_thing)
-
-
-    if __name__ == "__main__":
-        # This runs all 4 combinations of success/failure.
-        # The output will be the Ok result or first error.
-        for is1, is2 in itertools.product([True, False], repeat=2):
-            the_result: Result[int, str] = result_sum(is1, is2)
-            print(f"{the_result=}")
-
-        # Output: ```
-            the_result=Ok(90)
-            the_result=Err('magnets2')
-            the_result=Err('magnets1')
-            the_result=Err('magnets1')
-        ```
-
-    """
-
-    @functools.wraps(func)
-    def wrapper(*args: PS.args, **kwargs: PS.kwargs) -> Result[TR, E]:
-        generator = func(*args, **kwargs)
-        try:
-            value = next(generator)
-            while True:
-                match value:
-                    case Ok(value_):
-                        value = generator.send(value_)
-                    case Err():
-                        return value
-        except StopIteration as e:
-            return e.value
-
-    return wrapper
-
-
-def result_reduce_list_all_ok(
-    lst: Iterable[Result[T, str]]
-) -> Result[list[T], str]:
+def result_reduce_list_all_ok(lst: Iterable[Result[T, str]]) -> Result[list[T], str]:
     oks, errs = result_reduce_list_separate(lst)
     if errs:
         return Err("\n".join(errs))
@@ -190,3 +171,75 @@ async def result_reduce_list_all_ok_async(
         return Err("\n".join(errs))
     else:
         return Ok(oks)
+
+
+def parametrized(
+    parametrized_decorator: Callable[
+        Concatenate[Callable[PS2, T_Return], PS], Callable[PS3, T_Return2]
+    ]
+) -> Callable[PS, Callable[[Callable[PS2, T_Return]], Callable[PS3, T_Return2]]]:
+    def make_decorator(
+        *args: PS.args, **kwargs: PS.kwargs
+    ) -> Callable[[Callable[PS2, T_Return]], Callable[PS3, T_Return2]]:
+        def decorator(func: Callable[PS2, T_Return]) -> Callable[PS3, T_Return2]:
+            return parametrized_decorator(func, *args, **kwargs)
+
+        return decorator
+
+    return make_decorator
+
+
+@parametrized
+def exception_handled(
+    fn: T_UnsafeFn[PS, T_Return],
+    exception_handler: Callable[[Exception], Result[T_Return, T_Err]],
+) -> T_SafeFn[PS, T_Return, T_Err]:
+    """
+    Parametrized decorator for handling exceptions. 
+    User defines an exception-handling function (`exception_handler`) that converts an Exception to a Result.
+    
+    Returns:
+        A plain decorator, i.e. you can use it with @ as follows:
+        ```
+        @exception_handled(my_exception_handler)
+        def my_raising_function(...):
+            raise Exception("Something went wrong")
+        ```
+    """
+    def decorated(*args: PS.args, **kwargs: PS.kwargs) -> Result[T_Return, T_Err]:
+        try:
+            return Ok(fn(*args, **kwargs))
+        except Exception as e:
+            return exception_handler(e)
+
+    return decorated
+
+
+@parametrized
+def exception_handled_async(
+    fn: T_AsyncUnsafeFn[PS, T_Return],
+    exception_handler: Callable[[Exception], Result[T_Return, T_Err]],
+) -> T_AsyncSafeFn[PS, T_Return, T_Err]:
+    async def decorated(*args: PS.args, **kwargs: PS.kwargs) -> Result[T_Return, T_Err]:
+        try:
+            return Ok(await fn(*args, **kwargs))
+        except Exception as e:
+            return exception_handler(e)
+
+    return decorated
+
+
+def wrap_exception_traceback(e: Exception) -> Err[str]:
+    return ErrWithTraceback(e)
+
+
+def wrap_exception(e: Exception) -> Err[Exception]:
+    return Err(e)
+
+
+exception_to_err_with_traceback = exception_handled(wrap_exception_traceback)
+exception_to_err = exception_handled(wrap_exception)
+exception_to_err_with_traceback_async = exception_handled_async(
+    wrap_exception_traceback
+)
+exception_to_err_async = exception_handled_async(wrap_exception)
